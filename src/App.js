e7499339668f7255d4406a071bfed08616bf7d2a
K Tact <ktact.dev@gmail.com>
2020-10-20 23:25:40 +0900

    formのサンプルとして住所検索アプリを実装

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipcode: '',
+      address: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ zipcode: e.target.value });
+  }
+
+  handleSubmit(e) {
+    fetch(`https://api.zipaddress.net/?zipcode=${this.state.zipcode}`, {
+      mode: 'cors'
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({ address: json.data.fullAddress });
+      });
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <input type="text" value={this.state.zipcode} onChange={this.handleChange}/>
+              <input type="submit" value="検索"/>
+            </p>
+          </form>
+          <p>{this.state.address}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
